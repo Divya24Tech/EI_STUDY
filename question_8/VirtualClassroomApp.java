bbbb479e3297956740842f6cabd759f56fbd0259
Divya <160032946+Divya24Tech@users.noreply.github.com>
2024-10-01 22:34:47 +0530

    Update VirtualClassroomApp.java

--- question_8/VirtualClassroomApp.java
+++ question_8/VirtualClassroomApp.java
@@ -1,1 +1,13 @@
 
+import manager.VirtualClassroomManager;
+import java.util.logging.Logger;
+
+public class VirtualClassroomApp {
+    private static final Logger LOGGER = Logger.getLogger(VirtualClassroomApp.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.info("Starting Virtual Classroom Manager...");
+        VirtualClassroomManager manager = new VirtualClassroomManager();
+        manager.start();
+    }
+}
