b3e87d36ab2b8625ec413e21e5435b23b41529c0
Divya <160032946+Divya24Tech@users.noreply.github.com>
2024-10-01 23:52:33 +0530

    Add files via upload

---
+++ question_3/mars_rover/commands/TurnLeftCommand.java
@@ -0,0 +1,17 @@
+
+package commands;
+
+import rover.Rover;
+
+public class TurnLeftCommand implements Command {
+    private Rover rover;
+
+    public TurnLeftCommand(Rover rover) {
+        this.rover = rover;
+    }
+
+    @Override
+    public void execute() {
+        rover.turnLeft();
+    }
+}
