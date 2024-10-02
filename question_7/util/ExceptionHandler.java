2e9cf3cf0831fd3884087496c5c2036c6f23e90d
Divya <160032946+Divya24Tech@users.noreply.github.com>
2024-10-02 08:30:07 +0530

    Delete question_7 directory

--- question_7/util/ExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public class ExceptionHandler {
-    public static void handleException(Exception e) {
-        LoggerUtil.logError("An error occurred: " + e.getMessage());
-        e.printStackTrace();
-    }
-}
