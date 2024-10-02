b7a48e6206c41b6ac4e40b45df1d31f185874d1a
Divya <160032946+Divya24Tech@users.noreply.github.com>
2024-10-02 08:28:45 +0530

    Delete question_1/astronaut_schedule directory

--- question_1/astronaut_schedule/ConflictObserver.java
+++
@@ -1,6 +0,0 @@
-public class ConflictObserver implements TaskObserver {
-    @Override
-    public void update(Task task) {
-        System.out.println("Conflict detected with task: " + task.getDescription());
-    }
-}
--- question_1/astronaut_schedule/exceptions/TaskConflictException.java
+++
@@ -1,5 +0,0 @@
-public class TaskConflictException extends Exception {
-    public TaskConflictException(String message) {
-        super(message);
-    }
-}
