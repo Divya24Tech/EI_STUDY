a6cfc26c6419b59c238c66520124d9978ba33fd9
Divya <160032946+Divya24Tech@users.noreply.github.com>
2024-10-02 08:37:58 +0530

    Delete Excercise_1_question_1 directory

--- Excercise_1_question_1/o behavioural_design_pattern/observer/Subject.java
+++
@@ -1,9 +0,0 @@
-package behavioral_patterns.observer;
-
-import java.util.List;
-
-public interface Subject {
-    void registerObserver(Observer observer);
-    void removeObserver(Observer observer);
-    void notifyObservers();
-}
