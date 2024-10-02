6226f51d1bf80909eaf2d783e6d86c7504971eb0
Divya <160032946+Divya24Tech@users.noreply.github.com>
2024-10-02 10:22:14 +0530

    Create Valodation.java

---
+++ Creational_design_pattern./Document_Generation_System/document/utilities/Valodation.java
@@ -0,0 +1,10 @@
+
+package Document_Generation_System.utilities;
+
+public class Validation {
+    public static void validateString(String str, String errorMessage) {
+        if (str == null || str.isEmpty()) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+}
