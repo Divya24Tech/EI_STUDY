ddd2075f118db80a7c5fde73afbffeb4d951997a
Divya <160032946+Divya24Tech@users.noreply.github.com>
2024-10-02 10:20:12 +0530

    Create WordDocument.java

---
+++ Creational_design_pattern./Document_Generation_System/document/factory/WordDocument.java
@@ -0,0 +1,12 @@
+
+package Document_Generation_System.document.factory;
+
+import Document_Generation_System.utilities.Logging;
+
+public class WordDocument extends Document {
+    @Override
+    public void generate() {
+        Logging.info("Generating Word document...");
+        // Word generation logic
+    }
+}
