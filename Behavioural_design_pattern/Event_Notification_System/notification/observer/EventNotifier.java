a4873f7346429634de0e2bee2380e573035fd298
Divya <160032946+Divya24Tech@users.noreply.github.com>
2024-10-02 09:00:08 +0530

    Create EventNotifier.java

---
+++ Behavioural_design_pattern/Event_Notification_System/notification/observer/EventNotifier.java
@@ -0,0 +1,33 @@
+package com.companyname.notification.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.companyname.utilities.Logging;
+
+public class EventNotifier implements Subject {
+    private List<Observer> observers = new ArrayList<>();
+
+    @Override
+    public void subscribe(Observer observer) {
+        observers.add(observer);
+        Logging.info("User subscribed to event notifications.");
+    }
+
+    @Override
+    public void unsubscribe(Observer observer) {
+        observers.remove(observer);
+        Logging.info("User unsubscribed from event notifications.");
+    }
+
+    @Override
+    public void notifyObservers(String eventMessage) {
+        for (Observer observer : observers) {
+            observer.update(eventMessage);
+        }
+    }
+
+    public void notifyEvent(String eventMessage) {
+        Logging.info("Event triggered: " + eventMessage);
+        notifyObservers(eventMessage);
+    }
+}
