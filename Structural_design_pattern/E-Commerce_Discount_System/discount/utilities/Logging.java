4046cc5efaa6309b6a9d0ef7b5f575a6d6f07d65
Divya <160032946+Divya24Tech@users.noreply.github.com>
2024-10-02 10:49:06 +0530

    Create Logging.java

---
+++ Structural_design_pattern/E-Commerce_Discount_System/discount/utilities/Logging.java
@@ -0,0 +1,14 @@
+
+package E-Commerce_Discount_System.utilities;
+
+import java.time.LocalDateTime;
+
+public class Logging {
+    public static void info(String message) {
+        System.out.println("INFO [" + LocalDateTime.now() + "]: " + message);
+    }
+
+    public static void error(String message) {
+        System.out.println("ERROR [" + LocalDateTime.now() + "]: " + message);
+    }
+}
