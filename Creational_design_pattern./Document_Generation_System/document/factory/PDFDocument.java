0cc45b03e966f6895ec89a3dd3ff8b72b93ecee1
Divya <160032946+Divya24Tech@users.noreply.github.com>
2024-10-02 10:19:13 +0530

    Create PDFDocument.java

---
+++ Creational_design_pattern./Document_Generation_System/document/factory/PDFDocument.java
@@ -0,0 +1,12 @@
+
+package Document_Generation_System.document.factory;
+
+import Document_Generation_System.utilities.Logging;
+
+public class PDFDocument extends Document {
+    @Override
+    public void generate() {
+        Logging.info("Generating PDF document...");
+        // PDF generation logic
+    }
+}
