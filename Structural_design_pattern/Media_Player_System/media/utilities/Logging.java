189196e5c1fd2a60a4b0875f5129b4281787f5ff
Divya <160032946+Divya24Tech@users.noreply.github.com>
2024-10-02 10:38:13 +0530

    Create Logging.java

---
+++ Structural_design_pattern/Media_Player_System/media/utilities/Logging.java
@@ -0,0 +1,14 @@
+
+package Media_Player_System.utilities;
+
+import java.time.LocalDateTime;
+
+public class Logging {
+    public static void info(String message) {
+        System.out.println("INFO [" + LocalDateTime.now() + "]: " + message);
+    }
+
+    public static void error(String message) {
+        System.out.println("ERROR [" + LocalDateTime.now() + "]: " + message);
+    }
+}
